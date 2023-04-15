60d24f1eb0b1195448a9e4e1db7bec90ea830545
grzesikb <grzesikb@wp.pl>
2023-04-16 00:27:02 +0200

    add Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,73 @@
-import { ThemeProvider } from "@emotion/react";
-import { AppBar, Toolbar, createTheme, Typography } from "@mui/material";
-import { ReactNode } from "react";
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
-interface NavbarProps {
-  children: ReactNode;
-}
+const Navbar = () => {
+  //  const [auth, setAuth] = useState(true);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
-const Navbar = ({ children }: NavbarProps) => {
-  const darkTheme = createTheme({
-    palette: {
-      primary: {
-        main: "rgb(27, 18, 18)",
-      },
-    },
-  });
+  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setAuth(event.target.checked);
+  // };
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <ThemeProvider theme={darkTheme}>
-      <AppBar position="static" color="primary" enableColorOnDark>
-        <Typography variant="h6" color="inherit" component="div">
-          EventsManagment
-        </Typography>
-        <Toolbar
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            height: "7vh",
-          }}
-        >
-          {children}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, fontWeight: 600, marginLeft: 1 }}
+          >
+            Evento
+          </Typography>
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
-    </ThemeProvider>
+    </Box>
   );
 };
-
 export default Navbar;
