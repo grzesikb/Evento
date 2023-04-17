327db52147c230a2e88b8ee0c7abea0901f5b11d
grzesikb <grzesikb@wp.pl>
2023-04-18 00:56:04 +0200

    Create molecule OrderEvents

---
+++ src/components/OrderEvents.tsx
@@ -0,0 +1,50 @@
+import { Box, Container, Grid, Paper, Typography } from '@mui/material';
+
+import EventTypeCard from './EventTypeCard';
+
+const OrderEvents = () => {
+  return (
+    <Container>
+      <Paper
+        variant="outlined"
+        sx={{ display: 'flex', padding: 6, marginTop: 5, borderRadius: 4 }}
+      >
+        <Grid container>
+          <Grid xs={12}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={{ marginBottom: 3 }}
+            >
+              Order an event. Choose a package
+            </Typography>
+          </Grid>
+          <Grid xs>
+            <EventTypeCard
+              src="./assets/Card-PublicParty.jpg"
+              header="Public Event"
+              additional="Dance parties, Concerts, Club events"
+            />
+          </Grid>
+          <Grid xs>
+            <EventTypeCard
+              src="./assets/Card-PrivateParty.jpg"
+              header="Private Event"
+              additional="Presentation, Conference for companies"
+            />
+          </Grid>
+          <Grid xs>
+            <EventTypeCard
+              src="./assets/Card-CelebrationParty.jpg"
+              header="Celebration Event"
+              additional="Birthdays, Name days, Bachelorette parties"
+            />
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
+  );
+};
+
+export default OrderEvents;
