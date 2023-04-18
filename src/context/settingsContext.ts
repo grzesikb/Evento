9191fb3d9b1700ee146bb54f4c58f438f9b0d57b
grzesikb <grzesikb@wp.pl>
2023-04-18 20:37:12 +0200

    add SettingsContext

--- src/context/settingsContext.ts
+++ src/context/settingsContext.ts
@@ -1,39 +1,12 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import { createContext, useReducer } from 'react';
 import { PaletteMode } from '@mui/material';
+import { createContext } from 'react';
 
 interface SettingsContextInterface {
   theme: PaletteMode | undefined;
-  setTheme: React.Dispatch<React.SetStateAction<string>>;
+  dispatch: React.Dispatch<any>;
 }
 
 export const SettingsContext = createContext<SettingsContextInterface>({
   theme: 'dark',
-  setTheme: () => {},
+  dispatch: () => null,
 });
-
-interface SetThemeAction {
-  type: 'SET_THEME';
-  payload: string;
-}
-
-const settingReducer = (state: string, action: SetThemeAction) => {
-  switch (action.type) {
-    case 'SET_THEME':
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const SettingsProvider: React.FC<{}> = ({ children }) => {
-  const [theme, dispatch] = useReducer(settingReducer, 'light'); // zmiana nazwy reduktora
-  const setTheme = (newTheme: string) =>
-    dispatch({ type: 'SET_THEME', payload: newTheme });
-
-  return (
-    <SettingsContext.Provider value={{ theme, setTheme }}>
-      {children}
-    </SettingsContext.Provider>
-  );
-};
