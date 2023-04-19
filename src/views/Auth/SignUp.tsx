d48a4f44bf58b4c68bbb764b7a5de310f64e327c
grzesikb <grzesikb@wp.pl>
2023-04-19 22:08:44 +0200

    Create SignUp page

---
+++ src/views/Auth/SignUp.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import { TextField } from '@mui/material';
+import Form from './AuthForm';
+
+const SignUp = () => {
+  const [data, setData] = useState<{ email: string; password: string }>({
+    email: '',
+    password: '',
+  });
+  // const errors = useValidate(data as LoginDataInterface, signInSchema);
+
+  // const { mutateAsync, isSuccess } = useMutation(signInService);
+  // const { state, dispatch } = useContext(AuthContext);
+  // const navigate = useNavigate();
+
+  // const onSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+  //   e.preventDefault();
+  //   const loginPromise = mutateAsync(data);
+
+  //   await statusNotifier<AxiosResponse>(loginPromise, {
+  //     pendingText: 'Logowanie...',
+  //     successText: 'Zalogowano!',
+  //     toastId,
+  //   })
+  //     .then((response: AxiosResponse) => {
+  //       dispatch({ type: ACTIONS.loadUser, payload: { ...response.data } });
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
+  const onSubmit = async () => {};
+  // if (state.user) {
+  // 	navigate('../../home');
+  // }
+  return (
+    <Form
+      handleClick={() => onSubmit()}
+      text="Utwórz konto"
+      navigateText="Posiadasz konto? Zaloguj się!"
+    >
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Adres email"
+        name="email"
+        autoComplete="email"
+        autoFocus
+        value={data.email}
+        onChange={(e) => setData({ ...data, email: e.target.value })}
+      />
+
+      <TextField
+        margin="dense"
+        required
+        fullWidth
+        name="password"
+        label="Hasło"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+        value={data.password}
+        onChange={(e) => setData({ ...data, password: e.target.value })}
+      />
+
+      <TextField
+        margin="dense"
+        required
+        fullWidth
+        name="password"
+        label="Powtórz hasło"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+        value={data.password}
+        onChange={(e) => setData({ ...data, password: e.target.value })}
+      />
+    </Form>
+  );
+};
+
+export default SignUp;
