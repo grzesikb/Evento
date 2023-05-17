e49b8c4b1cb22ec56cca7f6cb193327eb6ec3869
grzesikb <grzesikb@wp.pl>
2023-05-17 20:07:15 +0200

    fix Back

--- src/components/common/Back.tsx
+++ src/components/common/Back.tsx
@@ -1,20 +1,24 @@
 import { Chip } from '@mui/material';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { useNavigate } from 'react-router-dom';
 
 interface IProps {
-  onClick: () => any;
+  to: string;
 }
 
-const Back = ({ onClick }: IProps) => (
-  <div className="noSelect">
-    <Chip
-      label="Back"
-      onClick={() => onClick}
-      icon={<ChevronLeftIcon fontSize="small" />}
-      variant="outlined"
-      sx={{ mb: 3 }}
-    />
-  </div>
-);
+const Back = ({ to }: IProps) => {
+  const navigate = useNavigate();
+  return (
+    <div className="noSelect">
+      <Chip
+        label="Back"
+        onClick={() => navigate(to)}
+        icon={<ChevronLeftIcon fontSize="small" />}
+        variant="outlined"
+        sx={{ mb: 3 }}
+      />
+    </div>
+  );
+};
 
 export default Back;
