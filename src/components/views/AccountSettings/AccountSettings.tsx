18f9d84f68e47a7ec0ab0633a131be5db895d9ed
grzesikb <grzesikb@wp.pl>
2023-05-17 20:07:46 +0200

    fix and add AppContainer

--- src/components/views/AccountSettings/AccountSettings.tsx
+++ src/components/views/AccountSettings/AccountSettings.tsx
@@ -1,81 +1,56 @@
-import { Button, Grid, Paper, TextField, Typography } from '@mui/material';
+import { Button, Grid, TextField } from '@mui/material';
 import Box from '@mui/material/Box';
-import Back from '../../common/Back';
-import Navbar from '../../common/Navbar/Navbar';
 
-interface IProps {
-  onBack: () => any;
-}
+import AppContainer from '../../common/AppContainer';
 
-const AccountSettings = (props: IProps) => {
+const AccountSettings = () => {
   return (
-    <Box>
-      <Navbar />
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          mt: 12,
-        }}
-      >
-        <Paper variant="outlined" sx={{ padding: 6, borderRadius: 4 }}>
-          <Back onClick={props.onBack} />
-          <Typography
-            component="h1"
-            variant="h5"
-            sx={{ fontWeight: 600, mb: 2 }}
-          >
-            Account Settings
-          </Typography>
-          <Box component="form">
-            <Grid container sx={{ width: 400 }}>
-              <Grid item sm={12}>
-                <TextField
-                  margin="dense"
-                  required
-                  fullWidth
-                  id="oldPassword"
-                  label="Old password"
-                  name="oldPassword"
-                  autoFocus
+    <AppContainer back="/app/dashboard" label="Account Settings" navbar>
+      <Box component="form">
+        <Grid container sx={{ width: 400 }}>
+          <Grid item sm={12}>
+            <TextField
+              margin="dense"
+              required
+              fullWidth
+              id="oldPassword"
+              label="Old password"
+              name="oldPassword"
+              autoFocus
 
-                  // helperText="Please enter your name"
-                  // value={}
-                  // onChange={() => {}}
-                />
-              </Grid>
-              <Grid item sm={12}>
-                <TextField
-                  margin="dense"
-                  required
-                  fullWidth
-                  id="newPassword"
-                  label="New password"
-                  name="newPassword"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item sm={12}>
-                <TextField
-                  margin="dense"
-                  required
-                  fullWidth
-                  id="repeatNewPassword"
-                  label="Repeat new password"
-                  name="repeatNewPassword"
-                  autoFocus
-                />
-              </Grid>
-            </Grid>
-            <Button variant="contained" sx={{ fontWeight: 600, mt: 3 }}>
-              Reset password
-            </Button>
-          </Box>
-        </Paper>
+              // helperText="Please enter your name"
+              // value={}
+              // onChange={() => {}}
+            />
+          </Grid>
+          <Grid item sm={12}>
+            <TextField
+              margin="dense"
+              required
+              fullWidth
+              id="newPassword"
+              label="New password"
+              name="newPassword"
+              autoFocus
+            />
+          </Grid>
+          <Grid item sm={12}>
+            <TextField
+              margin="dense"
+              required
+              fullWidth
+              id="repeatNewPassword"
+              label="Repeat new password"
+              name="repeatNewPassword"
+              autoFocus
+            />
+          </Grid>
+        </Grid>
+        <Button variant="contained" sx={{ fontWeight: 600, mt: 3 }}>
+          Reset password
+        </Button>
       </Box>
-    </Box>
+    </AppContainer>
   );
 };
 
