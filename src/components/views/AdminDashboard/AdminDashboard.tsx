626264393f63699f8e2ccfe6911f0a343629b96e
grzesikb <grzesikb@wp.pl>
2023-05-27 18:12:22 +0200

    add primary handlers and setStates in AdminDashboard

--- src/components/views/AdminDashboard/AdminDashboard.tsx
+++ src/components/views/AdminDashboard/AdminDashboard.tsx
@@ -13,12 +13,23 @@ import {
 import Navbar from '../../common/Navbar/Navbar';
 import Alert from '../../common/Alert';
 import WorkerList from './WorkerList';
+import { IWorkersList } from '../../../shared/interfaces/admin.interface';
 
 const AdminDashboard = () => {
   const theme = useTheme();
   const [openDialog, setOpenDialog] = useState<boolean>(false);
 
-  const handleClose = () => {
+  const [worker, setWorker] = useState<IWorkersList>({
+    email: '',
+    firstName: '',
+    lastName: '',
+  });
+
+  const handleCloseDialog = async () => {
+    setOpenDialog(false);
+  };
+
+  const handleAddWorker = async () => {
     setOpenDialog(false);
   };
 
@@ -36,46 +47,53 @@ const AdminDashboard = () => {
         >
           Add Worker
         </Button>
-        <Dialog open={openDialog} onClose={handleClose}>
+        <Dialog open={openDialog} onClose={handleCloseDialog}>
           <DialogTitle>Add new worker</DialogTitle>
           <DialogContent>
             <TextField
-              autoFocus
               margin="dense"
               id="email"
               label="Email Address"
               type="email"
               fullWidth
               required
+              value={worker.email}
+              onChange={(e) => setWorker({ ...worker, email: e.target.value })}
             />
             <TextField
-              autoFocus
               margin="dense"
               id="firstName"
               label="First Name"
               type="firstName"
               fullWidth
               required
+              value={worker.firstName}
+              onChange={(e) =>
+                setWorker({ ...worker, firstName: e.target.value })
+              }
             />
             <TextField
-              autoFocus
               margin="dense"
               id="lastName"
               label="Last Name"
               type="lastName"
               fullWidth
               required
+              value={worker.lastName}
+              onChange={(e) =>
+                setWorker({ ...worker, lastName: e.target.value })
+              }
             />
           </DialogContent>
           <DialogActions>
             <Button
-              onClick={handleClose}
+              onClick={handleCloseDialog}
               sx={{ color: theme.palette.mode === 'dark' ? '#fff' : '#000' }}
             >
               Cancel
             </Button>
             <Button
-              onClick={handleClose}
+              onClick={handleAddWorker}
               sx={{ color: theme.palette.mode === 'dark' ? '#fff' : '#000' }}
             >
               Add
