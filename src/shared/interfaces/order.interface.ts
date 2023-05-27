60ab35169d42238808ed785d47dd25e2c394a251
grzesikb <grzesikb@wp.pl>
2023-05-27 19:48:57 +0200

    delete old trash and add IOrderDatesProps

--- src/shared/interfaces/order.interface.ts
+++ src/shared/interfaces/order.interface.ts
@@ -17,33 +17,9 @@ export interface IOrder {
   types?: 'Birthdays' | 'Name days' | 'Bachelorette parties' | '';
 }
 
-export interface IOrderEvent {
-  name: string;
-  startDate: string | null | undefined;
-  finishDate: string | null | undefined;
-  type: string;
-  status: string;
-  additionalInfo?: string;
-  securityOption?: boolean;
-  barOption?: boolean;
-}
-
-export interface IOrderPublicEvent {
-  artist: string;
-  maxPeople: number;
-  minAge: number;
-}
-
-export interface IOrderPrivateEvent {
-  numberOfSeats: number;
-  companyName: string;
-  cateringOption?: boolean;
-}
-
-export interface IOrderCelebrationEvent {
-  numberOfSeatsC: number;
-  cateringName: string;
-  types: 'Birthdays' | 'Name days' | 'Bachelorette parties';
+export interface IOrderDatesProps {
+  startDate?: string | null | undefined;
+  finishDate?: string | null | undefined;
 }
 
 export interface IOrderEventsDataGrid {
