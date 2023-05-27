ba826c7e68065b2716869046605a166c7a0546ad
grzesikb <grzesikb@wp.pl>
2023-05-27 18:15:34 +0200

    add IWorkersList interface

--- src/shared/interfaces/admin.interface.ts
+++ src/shared/interfaces/admin.interface.ts
@@ -2,3 +2,9 @@ export interface IEmailsApp {
   securityEmail: string;
   accountantEmail: string;
 }
+
+export interface IWorkersList {
+  email: string;
+  firstName: string;
+  lastName: string;
+}
