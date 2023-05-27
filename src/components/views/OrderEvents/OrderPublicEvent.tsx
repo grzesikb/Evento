f8f3cbcda3638d5e3b157d5e251fef27c64bed73
grzesikb <grzesikb@wp.pl>
2023-05-27 19:47:51 +0200

    add primary handlers and setStates in OrderPublicEvent

--- src/components/views/OrderEvents/OrderPublicEvent.tsx
+++ src/components/views/OrderEvents/OrderPublicEvent.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
   Box,
   Button,
@@ -9,11 +10,40 @@ import {
   Typography,
 } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
-import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import AppContainer from '../../common/AppContainer';
+import {
+  IOrder,
+  IOrderDatesProps,
+} from '../../../shared/interfaces/order.interface';
 
-const OrderPublicEvent = () => {
+const OrderPublicEvent = (props: IOrderDatesProps) => {
+  const [data, setData] = useState<IOrder>({
+    name: '',
+    startDate: props.startDate,
+    finishDate: props.finishDate,
+    type: '',
+    status: '',
+    additionalInfo: '',
+    securityOption: true,
+    barOption: false,
+    artist: '',
+    maxPeople: '',
+    minAge: '',
+    numberOfSeats: '',
+    companyName: '',
+    cateringOption: false,
+    cateringName: '',
+    types: '',
+  });
+
+  const navigate = useNavigate();
+  const handleOrderEvent = async () => {
+    // add order
+    navigate('/app/dashboard');
+  };
+
   return (
     <AppContainer
       back="/app/dashboard"
@@ -21,6 +51,19 @@ const OrderPublicEvent = () => {
       additionalLabel="Dance parties, Concerts, Club events"
       navbar
     >
+      <Typography
+        component="h2"
+        variant="h5"
+        sx={{
+          fontWeight: 400,
+          mb: 1,
+          fontSize: 15,
+          color: 'grey',
+        }}
+      >
+        {`Selected dates: ${props.startDate} - ${props.finishDate}`}
+      </Typography>
+
       <Box component="form">
         <Grid container>
           <Grid item sm={12}>
@@ -28,9 +71,11 @@ const OrderPublicEvent = () => {
               margin="dense"
               required
               fullWidth
-              id="eventName"
+              id="name"
               label="Event Name"
-              name="eventName"
+              name="name"
+              value={data.name}
+              onChange={(e) => setData({ ...data, name: e.target.value })}
             />
           </Grid>
           <Grid item sm={12}>
@@ -43,8 +88,8 @@ const OrderPublicEvent = () => {
               name="artist"
               helperText="If you want to order artists, enter their names and artistic pseudonym"
               multiline
-              // value={}
-              // onChange={() => {}}
+              value={data.artist}
+              onChange={(e) => setData({ ...data, artist: e.target.value })}
             />
           </Grid>
 
@@ -53,9 +98,11 @@ const OrderPublicEvent = () => {
               margin="dense"
               required
               fullWidth
-              id="numberOfPeople"
+              id="maxPeople"
               label="Maximum number of people"
-              name="numberOfPeople"
+              name="maxPeople"
+              value={data.maxPeople}
+              onChange={(e) => setData({ ...data, maxPeople: e.target.value })}
             />
           </Grid>
           <Grid item sm={1}></Grid>
@@ -67,6 +114,8 @@ const OrderPublicEvent = () => {
               label="Minimal Age"
               name="minAge"
               helperText="Minimum age of a person to let him or her into the party"
+              value={data.minAge}
+              onChange={(e) => setData({ ...data, minAge: e.target.value })}
             />
           </Grid>
           <Grid item sm={12}>
@@ -77,19 +126,35 @@ const OrderPublicEvent = () => {
               label="Aditional info / Expectations"
               name="additionalInfo"
               multiline
+              value={data.additionalInfo}
+              onChange={(e) =>
+                setData({ ...data, additionalInfo: e.target.value })
+              }
             />
           </Grid>
           <Grid item sm={12}>
             <FormGroup className="noSelect">
               <FormControlLabel
                 sx={{ mt: 5 }}
-                control={<Checkbox color="success" />}
+                control={
+                  <Checkbox
+                    value={data.barOption}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      setData({
+                        ...data,
+                        barOption: event.target.checked,
+                      })
+                    }
+                    color="success"
+                  />
+                }
                 label="Bar option with bartending service"
               />
 
               <FormControlLabel
                 control={<Checkbox color="success" disabled checked />}
                 label="* Security and bodyguards"
+                value={data.securityOption}
               />
             </FormGroup>
           </Grid>
@@ -104,6 +169,7 @@ const OrderPublicEvent = () => {
           variant="contained"
           endIcon={<SendIcon />}
           sx={{ fontWeight: 600 }}
+          onClick={handleOrderEvent}
         >
           Order public event
         </Button>
