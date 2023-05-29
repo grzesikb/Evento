1862a1c21659642cd76adb590b0aa6aee08cd3c3
Wronqa <kuba.wrona@onet.pl>
2023-05-29 17:19:01 +0200

    Created new interfaces for user context

---
+++ src/shared/interfaces/user.interface.ts
@@ -0,0 +1,36 @@
+import { Dispatch } from 'react';
+
+export interface IUserContext {
+	state: IUserState | null;
+	dispatch: Dispatch<any>;
+}
+export interface IUserState {
+	user: IUser | null;
+}
+export interface IUser {
+	id: string;
+	email: string;
+	role: number;
+	address_data: IAddressData;
+	personal_data: IPersonalData;
+}
+export interface IAddressData {
+	city: string;
+	country: string;
+	house_number: string;
+	postal_code: string;
+	street: string;
+	voivodeship: string;
+}
+export interface IPersonalData {
+	first_name: string;
+	last_name: string;
+	phone: string;
+}
+export interface IUserAction {
+	type: UserActions;
+	payload: IUser;
+}
+export enum UserActions {
+	LOAD_USER = 'loadUser',
+}
