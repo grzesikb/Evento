4576f7cd3bea0f4a81e07ea77f711fc229f794c5
Wronqa <kuba.wrona@onet.pl>
2023-05-29 16:44:45 +0200

    Created registration system

--- src/components/views/Auth/SignUp.tsx
+++ src/components/views/Auth/SignUp.tsx
@@ -6,85 +6,85 @@ import Navbar from '../../common/Navbar/Navbar';
 import { IAuthSignIn } from '../../../shared/interfaces/auth.interface';
 
 const SignUp = () => {
-  const navigate = useNavigate();
-  const [data, setData] = useState<IAuthSignIn>({
-    email: '',
-    password: '',
-    repeatPassword: '',
-  });
-  // const errors = useValidate(data as LoginDataInterface, signInSchema);
+	const navigate = useNavigate();
+	const [data, setData] = useState<IAuthSignIn>({
+		email: '',
+		password: '',
+		repeatPassword: '',
+	});
+	// const errors = useValidate(data as LoginDataInterface, signInSchema);
 
-  // const { mutateAsync, isSuccess } = useMutation(signInService);
-  // const { state, dispatch } = useContext(AuthContext);
-  // const navigate = useNavigate();
+	// const { mutateAsync, isSuccess } = useMutation(signInService);
+	// const { state, dispatch } = useContext(AuthContext);
+	// const navigate = useNavigate();
 
-  // const onSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   e.preventDefault();
-  //   const loginPromise = mutateAsync(data);
+	// const onSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+	//   e.preventDefault();
+	//   const loginPromise = mutateAsync(data);
 
-  //   await statusNotifier<AxiosResponse>(loginPromise, {
-  //     pendingText: 'Logowanie...',
-  //     successText: 'Zalogowano!',
-  //     toastId,
-  //   })
-  //     .then((response: AxiosResponse) => {
-  //       dispatch({ type: ACTIONS.loadUser, payload: { ...response.data } });
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
-  const onSubmit = async () => {
-    console.log(data);
-    navigate('/auth/add-user-data');
-  };
+	//   await statusNotifier<AxiosResponse>(loginPromise, {
+	//     pendingText: 'Logowanie...',
+	//     successText: 'Zalogowano!',
+	//     toastId,
+	//   })
+	//     .then((response: AxiosResponse) => {
+	//       dispatch({ type: ACTIONS.loadUser, payload: { ...response.data } });
+	//     })
+	//     .catch((err) => console.log(err));
+	// };
+	const onSubmit = async () => {
+		console.log(data);
+		navigate('/auth/add-user-data' as never, { state: data } as never);
+	};
 
-  return (
-    <Box>
-      <Navbar hideMenu />
-      <Form
-        handleClick={() => onSubmit()}
-        text="Sign Up"
-        navigateText="Do you have an account? Login now!"
-        navigatePath="../auth/signin"
-      >
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-          value={data.email}
-          onChange={(e) => setData({ ...data, email: e.target.value })}
-        />
+	return (
+		<Box>
+			<Navbar hideMenu />
+			<Form
+				handleClick={() => onSubmit()}
+				text="Sign Up"
+				navigateText="Do you have an account? Login now!"
+				navigatePath="../auth/signin"
+			>
+				<TextField
+					margin="normal"
+					required
+					fullWidth
+					id="email"
+					label="Email address"
+					name="email"
+					autoComplete="email"
+					autoFocus
+					value={data.email}
+					onChange={(e) => setData({ ...data, email: e.target.value })}
+				/>
 
-        <TextField
-          margin="dense"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          value={data.password}
-          onChange={(e) => setData({ ...data, password: e.target.value })}
-        />
+				<TextField
+					margin="dense"
+					required
+					fullWidth
+					name="password"
+					label="Password"
+					type="password"
+					id="password"
+					value={data.password}
+					onChange={(e) => setData({ ...data, password: e.target.value })}
+				/>
 
-        <TextField
-          margin="dense"
-          required
-          fullWidth
-          name="repeatPassword"
-          label="Repeat your password"
-          type="password"
-          id="repeatPassword"
-          value={data.repeatPassword}
-          onChange={(e) => setData({ ...data, repeatPassword: e.target.value })}
-        />
-      </Form>
-    </Box>
-  );
+				<TextField
+					margin="dense"
+					required
+					fullWidth
+					name="repeatPassword"
+					label="Repeat your password"
+					type="password"
+					id="repeatPassword"
+					value={data.repeatPassword}
+					onChange={(e) => setData({ ...data, repeatPassword: e.target.value })}
+				/>
+			</Form>
+		</Box>
+	);
 };
 
 export default SignUp;
