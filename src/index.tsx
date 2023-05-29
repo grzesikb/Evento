ec3a6c02b4507d22b19e4e2a8bdc00bb782da213
Wronqa <kuba.wrona@onet.pl>
2023-05-29 16:44:17 +0200

    Added react query provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { SettingsProvider } from './contexts/provider/SettingsProvider';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+	document.getElementById('root') as HTMLElement
 );
 
+const queryClient = new QueryClient();
+
 root.render(
-  <React.StrictMode>
-    <SettingsProvider>
-      <App />
-    </SettingsProvider>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<SettingsProvider>
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
+		</SettingsProvider>
+	</React.StrictMode>
 );
