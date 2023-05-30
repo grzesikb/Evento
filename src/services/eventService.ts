e50646d5daa4864b6f90e49f1c98e462d3f54967
Wronqa <kuba.wrona@onet.pl>
2023-05-30 20:25:10 +0200

    Created new services

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -3,10 +3,32 @@ import { Api } from '../tools/Api';
 export const userEventsService = async (access_token: string) => {
 	return Api.getUserEvents(access_token);
 };
+
 export const createEventService = async (data: {
 	access_token: string;
 	orderData: any;
 }) => {
 	console.log(data);
 	return Api.createEvent(data.access_token, data.orderData);
+};
+
+export const eventDetailService = async (data: {
+	access_token: string;
+	id: string;
+}) => {
+	return Api.getEventDetails(data.access_token, data.id);
+};
+
+export const updateEventService = async (data: {
+	access_token: string;
+	orderData: any;
+}) => {
+	return Api.updateEvent(data.access_token, data.orderData);
+};
+
+export const deleteEventService = async (data: {
+	access_token: string;
+	id: string;
+}) => {
+	return Api.deleteEvent(data.access_token, data.id);
 };
