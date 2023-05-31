c9b305fb852c1971bda87b949e9e9b792333001c
wych0 <wycho01@wp.pl>
2023-05-31 12:57:33 +0200

    validating password and repeatPassword

--- src/tools/Validator.ts
+++ src/tools/Validator.ts
@@ -12,4 +12,28 @@ export class Validator {
         }
 		return message;
 	}
+
+    static async checkPassword(password: string, register: boolean): Promise<string | null>{
+        let message: string | null = null;
+        if(validator.isEmpty(password)){
+            message = 'Password is required';
+            return message;
+        } else if(register && !validator.isStrongPassword(password)){
+            message = 'Weak password';
+            return message;
+        }
+        return message;
+    }
+
+    static async checkRepeatPassword(password: string, repeatPassword: string): Promise<string | null>{
+        let message: string | null = null;
+        if(validator.isEmpty(repeatPassword)){
+            message = 'repeatPassword is required';
+            return message;
+        } else if(password !== repeatPassword){
+            message = 'passwords are not equal';
+            return message;
+        }
+        return message;
+    }
 }
