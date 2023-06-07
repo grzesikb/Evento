aff0e0cebc1b598cd64e850390df0a58665e6539
Wronqa <kuba.wrona@onet.pl>
2023-06-07 17:10:37 +0200

    Created new service for changing password

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -6,3 +6,10 @@ export const editUserProfileService = async (data: {
 }) => {
 	return Api.editUserProfile(data.access_token, data.userData);
 };
+
+export const changePasswordService = async (data: {
+	access_token: string;
+	userData: any;
+}) => {
+	return Api.changePassword(data.access_token, data.userData);
+};
