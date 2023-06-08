5186817e236ef2216dd252ea8326d30a36ac53a8
Wronqa <kuba.wrona@onet.pl>
2023-06-08 19:08:02 +0200

    Added page for payment success

---
+++ src/components/views/PaymentStatus/Success.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Success = () => {
+	return (
+		<div>
+			<h1>
+				Płaność przeszła, grzesik zrobie informacje że sie udało i tutaj będą
+				szczegóły płatności i wszystkie informacje o tej płatnosci
+			</h1>
+		</div>
+	);
+};
+
+export default Success;
