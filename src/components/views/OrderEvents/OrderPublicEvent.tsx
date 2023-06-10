d4b10a7e77149fc58bdfc82232517c0980095867
wych0 <wycho01@wp.pl>
2023-06-10 17:37:53 +0200

    validation

--- src/components/views/OrderEvents/OrderPublicEvent.tsx
+++ src/components/views/OrderEvents/OrderPublicEvent.tsx
@@ -23,6 +23,7 @@ import {
 	createEventService,
 	userEventsService,
 } from '../../../services/eventService';
+import { Validator } from '../../../tools/Validator';
 
 const OrderPublicEvent = (props: IOrderDatesProps) => {
 	const [data, setData] = useState<IOrder>({
@@ -46,6 +47,26 @@ const OrderPublicEvent = (props: IOrderDatesProps) => {
 	const navigate = useNavigate();
 	const { mutate, isSuccess } = useMutation(createEventService);
 
+	const [errors, setErrors] = useState({
+		name: '',
+		maxPeople: '',
+		age: ''
+	});
+
+	const validateForm = async () => {
+		const nameError = await Validator.checkRequiredString(data.name);
+		const maxPeopleError = await Validator.checkRequiredNumber(data.maxPeople, 'public');
+		const ageError = data.minAge ? await Validator.checkAge(data.minAge) : '';
+		
+		
+		setErrors({
+			name: nameError ?? '',
+			maxPeople: maxPeopleError ?? '',
+			age: ageError ?? '',
+		})
+		return !(nameError || maxPeopleError || ageError)
+	};
+
 	const handleOrderEvent = async () => {
 		const orderEntity = {
 			name: data.name,
@@ -65,11 +86,12 @@ const OrderPublicEvent = (props: IOrderDatesProps) => {
 		console.log(orderEntity.start_date)
 
 		// add order
-		mutate({
-			access_token: localStorage.getItem('accessToken') as string,
-			orderData: orderEntity,
-		});
-		///navigate('/app/dashboard');
+		if(await validateForm()){
+			mutate({
+				access_token: localStorage.getItem('accessToken') as string,
+				orderData: orderEntity,
+			});
+		}
 	};
 
 	useEffect(() => {
@@ -117,12 +139,13 @@ const OrderPublicEvent = (props: IOrderDatesProps) => {
 							name="name"
 							value={data.name}
 							onChange={(e) => setData({ ...data, name: e.target.value })}
+							error={!!errors.name}
+							helperText={errors.name}
 						/>
 					</Grid>
 					<Grid item sm={12}>
 						<TextField
 							margin="dense"
-							required
 							fullWidth
 							id="artist"
 							label="Artist Names"
@@ -144,6 +167,8 @@ const OrderPublicEvent = (props: IOrderDatesProps) => {
 							name="maxPeople"
 							value={data.maxPeople}
 							onChange={(e) => setData({ ...data, maxPeople: e.target.value })}
+							error={!!errors.maxPeople}
+							helperText={errors.maxPeople}
 						/>
 					</Grid>
 					<Grid item sm={1}></Grid>
@@ -154,9 +179,10 @@ const OrderPublicEvent = (props: IOrderDatesProps) => {
 							id="minAge"
 							label="Minimal Age"
 							name="minAge"
-							helperText="Minimum age of a person to let him or her into the party"
 							value={data.minAge}
 							onChange={(e) => setData({ ...data, minAge: e.target.value })}
+							error={!!errors.age}
+							helperText={errors.age ? errors.age : "Minimum age of a person to let him or her into the party"}
 						/>
 					</Grid>
 					<Grid item sm={12}>
