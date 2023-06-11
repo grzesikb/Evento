56db52d0312ac056f79feceb6ab9c2e3fedf1fef
pwgawzynski <pgawzynski.backup@gmail.com>
2023-06-11 17:12:59 +0200

    added invoice interface

---
+++ src/shared/interfaces/invoiceItem.interface.tsx
@@ -0,0 +1,13 @@
+export interface InvoiceItemI{
+    invoice_id: string;
+    name: string;
+    count: string | undefined;
+    price_per_item: string | undefined;
+}
+
+export interface InvoiceItemCreateI {
+    invoice_id: string;
+    name: string;
+    count: number | undefined;
+    price_per_item: number | undefined;
+}
