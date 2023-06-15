5cf667859865fe52a7c0c6f7ae8310c307d48e3a
Wronqa <kuba.wrona@onet.pl>
2023-06-15 23:57:26 +0200

    Fixed types

--- src/shared/interfaces/admin.interface.ts
+++ src/shared/interfaces/admin.interface.ts
@@ -1,10 +1,11 @@
 export interface IEmailsApp {
-  securityEmail: string;
-  accountantEmail: string;
+	securityEmail: string;
+	accountantEmail: string;
 }
 
 export interface IWorkersList {
-  email: string;
-  firstName: string;
-  lastName: string;
+	email: string;
+	firstName: string;
+	lastName: string;
+	password: string;
 }
