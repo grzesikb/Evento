16362f23ed7d218a27ad960e557d335f24d88a2d
Wronqa <kuba.wrona@onet.pl>
2023-06-15 23:57:19 +0200

    Created new services

---
+++ src/services/workerService.ts
@@ -0,0 +1,20 @@
+import { Api } from '../tools/Api';
+
+export const createWorkerService = async (data: {
+	accessToken: string;
+	data: any;
+}) => {
+	return Api.createWorker(data.accessToken, data.data);
+};
+
+export const getWorkersService = async (accessToken: string) => {
+	return Api.getWorker(accessToken);
+};
+
+export const deleteWorkerService = async (data: {
+	accessToken: string;
+	id: string;
+}) => {
+	console.log(data.id);
+	return Api.deleteWorker(data.accessToken, data.id);
+};
