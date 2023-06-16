1512b770077bebaf64aecd341f3df9166c9d16a5
grzesikb <grzesikb@wp.pl>
2023-06-16 17:25:11 +0200

    add  primary statuses

--- src/tools/StatusFormatter.ts
+++ src/tools/StatusFormatter.ts
@@ -1,9 +1,17 @@
 export const statusFormatter = (statusNumber: number) => {
-	switch (statusNumber) {
-		case 1:
-			return 'inProgress';
+  switch (statusNumber) {
+    case 1:
+      return "inProgress";
 
-		case 2:
-			return 'Verification';
-	}
+    case 2:
+      return "Verification";
+  }
 };
+
+export const statuses = [
+  "inProgress",
+  "Verification",
+  "Payments",
+  "Offer",
+  "Finished",
+];
