9255f3975d73a544f572d3e244ef5f1009987205
grzesikb <grzesikb@wp.pl>
2023-06-16 17:23:48 +0200

    statuschip

--- src/components/common/StatusChip.tsx
+++ src/components/common/StatusChip.tsx
@@ -1,5 +1,5 @@
-import { Chip } from '@mui/material';
-import React from 'react';
+import { Chip } from "@mui/material";
+import React from "react";
 
 interface IProps {
   type: string;
@@ -7,29 +7,29 @@ interface IProps {
 const StatusChip = ({ type }: IProps) => {
   return (
     <div>
-      {type === 'inProgress' && (
+      {type === "inProgress" && (
         <Chip label="In progress" size="small" color="success" />
       )}
-      {type === 'Verification' && (
+      {type === "Verification" && (
         <Chip label="Waiting for verification" size="small" color="warning" />
       )}
-      {type === 'Payments' && (
+      {type === "Payments" && (
         <Chip
           label="Pending payment"
           size="small"
           color="primary"
-          sx={{ backgroundColor: 'dodgerblue' }}
+          sx={{ backgroundColor: "dodgerblue" }}
         />
       )}
-      {type === 'Offer' && (
+      {type === "Offer" && (
         <Chip
           label="Pending Offer Confirmation"
           size="small"
           color="primary"
-          sx={{ backgroundColor: 'firebrick' }}
+          sx={{ backgroundColor: "firebrick" }}
         />
       )}
-      {type === 'Finished' && <Chip label="Finished" size="small" />}
+      {type === "Finished" && <Chip label="Finished" size="small" />}
     </div>
   );
 };
