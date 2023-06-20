1f7703268adfe410e143baca6dcc10f7090cd268
wych0 <wycho01@wp.pl>
2023-06-20 15:31:16 +0200

    code refactoring

--- src/components/views/WorkerDashboard/WorkerDashboard.tsx
+++ src/components/views/WorkerDashboard/WorkerDashboard.tsx
@@ -6,7 +6,7 @@ import EventList from './EventList';
 const WorkerDashboard = () => {
   return (
     <Box>
-      <Navbar email="worker@gmail.com" permission="Worker" />
+      <Navbar permission="Worker" />
       <Paper
         variant="outlined"
         sx={{ padding: 3, marginTop: 10, borderRadius: 4 }}
